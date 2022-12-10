eb14fb10b4453e11005cf502e1e4e5ad1fa1b375
Ryland DeGregory <rylanddegregory@me.com>
2022-12-09 21:06:55 -0500

    Update Functions to simplify code and eliminate Az PowerShell dependency

--- WeeklyCosmosPlaylistUpdate/run.ps1
+++ WeeklyCosmosPlaylistUpdate/run.ps1
@@ -1,8 +1,8 @@
 <#
     .SYNOPSIS
-        Export Spotify user playlists to an Azure CosmosDB collection
+        Export Spotify playlists to an Azure CosmosDB collection
     .DESCRIPTION
-        Export Spotify user playlists to an Azure CosmosDB using the Spotify web API with OAuth2 Client Authorization flow
+        Export Spotify playlists to an Azure CosmosDB using the Spotify web API with OAuth2 Client Authorization flow
     .NOTES
         - Assumes that a Spotify application has been configured and an OAuth2 Refresh token has been granted for a user
           https://developer.spotify.com/documentation/general/guides/authorization-guide/
@@ -14,83 +14,49 @@
 #region Init
 param ($Timer)
 
-# Properties that will be returned for each track
+# Ensure Function stops if an error is encountered
+$ErrorActionPreference = 'Stop'
+
+# Set Application credentials from Application Settings
+$ApplicationCredentials = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$($env:SPOTIFY_CLIENT_ID)`:$($env:SPOTIFY_CLIENT_SECRET)"))
+
+# Set Properties that will be returned for each track
 # https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-playlists-tracks
 $TrackFields = 'items(added_at,added_by.id,track(name,id,external_urls(spotify),artists(name,external_urls(spotify)),album(name,external_urls(spotify))))'
-
-# User-defined variables
-$KeyVaultName = $env:KEY_VAULT_NAME
-$PlaylistType = 'User'
 #endregion Init
 
-#region Functions
-function Get-SpotifyAccessToken {
-    <#
-        .SYNOPSIS
-            Get an OAuth2 Access token from an OAuth2 Refresh token and an application Client ID and Client Secret
-            OAuth2 Access token is used to access the Spotify API
-    #>
-    [CmdletBinding()]
-    param (
-        # The name of the Azure Key Vault where Spotify API secrets are stored
-        [Parameter(Mandatory)]
-        [string] $KeyVaultName
-    )
+#region GetAccessToken
+# Set Request elements
+$TokenHeader = @{ 'Authorization' = "Basic $ApplicationCredentials" }
+$TokenBody   = @{ grant_type = 'refresh_token'; refresh_token = "$($env:SPOTIFY_REFRESH_TOKEN)" }
+try {
+    # Get an Access token from the Refresh token
+    $AccessToken = Invoke-RestMethod -Method Post -Headers $TokenHeader -Uri 'https://accounts.spotify.com/api/token' -Body $TokenBody | Select-Object -ExpandProperty access_token
+} catch {
+    Write-Error "[ERROR] Error getting Access Token from Spotify API '/token' endpoint using Refresh Token: $_"
+}
 
-    begin {
-        # Application credentials
-        $ClientId               = Get-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName 'Spotify-ClientID' -AsPlainText
-        $ClientSecret           = Get-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName 'Spotify-ClientSecret' -AsPlainText
-        $ApplicationCredentials = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$ClientId`:$ClientSecret"))
-
-        # User credentials
-        $RefreshToken = Get-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName 'Spotify-RefreshToken' -AsPlainText
-    }
-
-    process {
-        # Request elements
-        $TokenHeader = @{ 'Authorization' = "Basic $ApplicationCredentials" }
-        $TokenBody   = @{ grant_type = 'refresh_token'; refresh_token = "$RefreshToken" }
-        try {
-            # Get an Access token from the Refresh token
-            $AccessToken = Invoke-RestMethod -Method Post -Headers $TokenHeader -Uri 'https://accounts.spotify.com/api/token' -Body $TokenBody
-        } catch {
-            throw "[ERROR] Error getting Access Token from Spotify API '/token' endpoint using Refresh Token: $($Error[0])"
-        }
-    }
-
-    end {
-        # If an Access token was granted, return it
-        if ($AccessToken) {
-            return $AccessToken.access_token
-       } else {
-           return $null
-       }
-    }
-} #endfunction Get-SpotifyAccessToken
-#endregion Functions
-
-#region GetPlaylists
 # Get OAuth2 Access token and set API headers
-$AccessToken = Get-SpotifyAccessToken -KeyVaultName $KeyVaultName
 if ($AccessToken) {
     $Headers = @{ 'Authorization' = "Bearer $AccessToken" }
 } else {
-    throw '[ERROR] No OAuth2 Access token was granted. Please ensure that the application ClientID and ClientSecret, and the user OAuth2 Refresh token are valid.'
+    Write-Error '[ERROR] No OAuth2 Access token was granted. Please ensure that the application ClientID and ClientSecret, and the user OAuth2 Refresh token are valid.'
 }
+#endregion GetAccessToken
 
+#region GetPlaylists
 try {
     # Get the authenticated user's Spotify profile
     $User = Invoke-RestMethod -Method Get -Headers $Headers -Uri 'https://api.spotify.com/v1/me/'
     Write-Host "Processing playlists for Spotify user $($User.display_name)"
 } catch {
-    throw "[ERROR] Error getting the authenticated user's Spotify profile: $($Error[0])"
+    Write-Error "[ERROR] Error getting the authenticated user's Spotify profile: $_"
 }
 # Determine the user's number of playlists and calculate the number of paginated requests to make
 try {
     $PlaylistCount = Invoke-RestMethod -Method Get -Headers $Headers -Uri 'https://api.spotify.com/v1/me/playlists?limit=1'
 } catch {
-    throw "[ERROR] Error getting the number of playlists for user: $($Error[0])"
+    Write-Error "[ERROR] Error getting the number of playlists for user: $_"
 }
 $PlaylistPages = [math]::ceiling($PlaylistCount.total / 50)
 
@@ -99,12 +65,12 @@ $Playlists = for ($i = 0; $i -lt $PlaylistPages; $i++) {
     try {
         Invoke-RestMethod -Method Get -Headers $Headers -Uri "https://api.spotify.com/v1/me/playlists?limit=50&offset=$($i * 50)"
     } catch {
-        throw "[ERROR] Error getting list of playlists for user: $($Error[0])"
+        Write-Error "[ERROR] Error getting list of playlists for user: $_"
     }
 }
 
 # Process playlist types based on type
-$ProcessPlaylists = switch ($PlaylistType) {
+$ProcessPlaylists = switch ($env:PLAYLIST_TYPE) {
     'User' { $Playlists.items | Where-Object { $_.owner.id -eq $User.id } }
     'Followed' { $Playlists.items | Where-Object { $_.owner.id -ne $User.id } }
     'All' { $Playlists.items }
@@ -123,7 +89,7 @@ $TrackArray = foreach ($Playlist in $ProcessPlaylists) {
             # Get all tracks in the playlist page, the API returns only the pre-defined fields
             $Tracks = Invoke-RestMethod -Method Get -Headers $Headers -Uri "https://api.spotify.com/v1/playlists/$($Playlist.id)/tracks?limit=100&offset=$($i * 100)&fields=$TrackFields"
         } catch {
-            throw "[ERROR] Error getting tracks from playlist [$($Playlist.name)]: $($Error[0])"
+            Write-Error "[ERROR] Error getting tracks from playlist [$($Playlist.name)]: $_"
         }
 
         # Create object for each track in playlist with processed data
@@ -147,5 +113,5 @@ $TrackArray = foreach ($Playlist in $ProcessPlaylists) {
 #endregion ProcessPlaylists
 
 #region Output
-Push-OutputBinding -Name outputDocument -Value $TrackArray
+Push-OutputBinding -Name OutputDocument -Value $TrackArray
 #endregion Output
