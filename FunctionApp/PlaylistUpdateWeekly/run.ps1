db57be8097d39d7c9b133c3936c241d71ed09566
Ryland DeGregory <ryland@ryland.dev>
2023-01-16 19:31:35 -0500

    Remove pagination in favor of while loop

--- FunctionApp/PlaylistUpdateWeekly/run.ps1
+++ FunctionApp/PlaylistUpdateWeekly/run.ps1
@@ -16,10 +16,6 @@ $ErrorActionPreference = 'Stop'
 # Spotify API config
 $SpotifyApiUrl = 'https://api.spotify.com/v1'
 $Headers = Get-SpotifyAccessToken
-
-# Properties that will be returned for each track
-# https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-playlists-tracks
-$TrackFields = 'items(added_at,added_by.id,track(name,id,external_urls(spotify),artists(name,external_urls(spotify)),album(name,external_urls(spotify))))'
 #endregion Init
 
 #region GetPlaylists
@@ -32,67 +28,58 @@ try {
 }
 
 try {
-    # Determine the user's number of playlists and calculate the number of paginated requests to make
-    $PlaylistCount = Invoke-RestMethod -Method Get -Headers $Headers -Uri "$SpotifyApiUrl/me/playlists?limit=1"
-    $PlaylistPages = [math]::ceiling($PlaylistCount.total / 50)
-    Write-Information "Library contains [$($PlaylistCount.total)] playlists"
-} catch {
-    Write-Error "Error getting the number of playlists for user [$UserDisplayName]: $_"
-}
-
-# Build collection of playlists by processing all pages
-$Playlists = for ($i = 0; $i -lt $PlaylistPages; $i++) {
-    try {
-        Write-Verbose "Processing playlist page [$i/$PlaylistPages]" -Verbose
-        Invoke-RestMethod -Method Get -Headers $Headers -Uri "$SpotifyApiUrl/me/playlists?limit=50&offset=$($i * 50)"
-    } catch {
-        Write-Error "Error getting list of playlists for user [$UserDisplayName]: $_"
+    # Get the user's list of playlists
+    $Response = @{
+        next = "$SpotifyApiUrl/me/playlists?limit=50"
     }
+    $Playlists = while ($Response.next) {
+        $Response = Invoke-RestMethod -Method Get -Headers $Headers -Uri $Response.next
+        $Response.items
+    }
+} catch {
+    Write-Error "Error getting list of followed artists for user [$UserDisplayName]: $_"
 }
 
-# Process playlist types based on type
+# Process playlist types based on App Setting
 $ProcessPlaylists = switch ($env:PLAYLIST_TYPE) {
-    'User' { $Playlists.items | Where-Object { $_.owner.id -eq $User.id } }
-    'Followed' { $Playlists.items | Where-Object { $_.owner.id -ne $User.id } }
-    'All' { $Playlists.items }
-    default { $Playlists.items }
+    'User' { $Playlists | Where-Object { $_.owner.id -eq $User.id } }
+    'Followed' { $Playlists | Where-Object { $_.owner.id -ne $User.id } }
+    'All' { $Playlists }
+    default { $Playlists }
 }
 Write-Information "Processing [$($ProcessPlaylists.Count)] [$($env:PLAYLIST_TYPE)] playlists"
 #endregion GetPlaylists
 
 #region ProcessPlaylists
+$PlaylistCount = 1
 $TrackArray = foreach ($Playlist in $ProcessPlaylists) {
-    $PlaylistCount = 1
     Write-Information "Processing playlist [$($Playlist.name)] with [$($Playlist.tracks.total)] tracks [$PlaylistCount/$($ProcessPlaylists.Count)]"
-    # Calculate the number of paginated requests to make to get all tracks in the playlist
-    $TrackPages = [math]::ceiling($Playlist.tracks.total / 100)
-
-    # Build collection of tracks by processing all pages
-    for ($i = 0; $i -lt $TrackPages; $i++) {
-        try {
-            Write-Verbose "Processing track page [$($i+1)/$TrackPages] in playlist [$($Playlist.name)]" -Verbose
-            # Get all tracks in the playlist page, the API returns only the pre-defined fields
-            $Tracks = Invoke-RestMethod -Method Get -Headers $Headers -Uri "$SpotifyApiUrl/playlists/$($Playlist.id)/tracks?limit=100&offset=$($i * 100)&fields=$TrackFields"
-        } catch {
-            Write-Error "Error getting tracks from playlist [$($Playlist.name)]: $_"
+    try {
+        # Get the user's list of playlists
+        $Response = @{
+            next = "$SpotifyApiUrl/playlists/$($Playlist.id)/tracks"
         }
-
-        foreach ($Track in $Tracks.items) {
-            [PSCustomObject]@{
-                PlaylistName  = $Playlist.name -replace '[^a-zA-Z0-9 -]', ''
-                PlaylistOwner = $Playlist.owner.id
-                PlaylistURL   = $Playlist.external_urls.spotify
-                AddedAt       = $Track.added_at
-                AddedBy       = $Track.added_by.id
-                Name          = $Track.track.name
-                TrackURL      = $Track.track.external_urls.spotify
-                Artist        = $Track.track.artists.name | Join-String -Separator ', '
-                ArtistURL     = $Track.track.artists.external_urls.spotify | Join-String -Separator ', '
-                Album         = $Track.track.album.name
-                AlbumURL      = $Track.track.album.external_urls.spotify
-                id            = "$($Playlist.id)_$($Track.track.id)"
+        while ($Response.next) {
+            $Response = Invoke-RestMethod -Method Get -Headers $Headers -Uri $Response.next
+            foreach ($Track in $Response.items) {
+                [PSCustomObject]@{
+                    PlaylistName  = $Playlist.name -replace '[^a-zA-Z0-9 -]', ''
+                    PlaylistOwner = $Playlist.owner.id
+                    PlaylistURL   = $Playlist.external_urls.spotify
+                    AddedAt       = $Track.added_at
+                    AddedBy       = $Track.added_by.id
+                    Name          = $Track.track.name
+                    TrackURL      = $Track.track.external_urls.spotify
+                    Artist        = $Track.track.artists.name | Join-String -Separator ', '
+                    ArtistURL     = $Track.track.artists.external_urls.spotify | Join-String -Separator ', '
+                    Album         = $Track.track.album.name
+                    AlbumURL      = $Track.track.album.external_urls.spotify
+                    id            = "$($Playlist.id)_$($Track.track.id)"
+                }
             }
         }
+    } catch {
+        Write-Error "Error getting list of tracks in Playlist [$($Playlist.name)]: $_"
     }
     $PlaylistCount++
 }
