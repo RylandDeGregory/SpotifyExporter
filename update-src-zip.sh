553261b79647850116703fea92d8578290c44745
Ryland DeGregory <18073815+RylandDeGregory@users.noreply.github.com>
2024-09-26 19:11:23 -0400

    Update src.zip and exclude update script from zip

--- update-src-zip.sh
+++ update-src-zip.sh
@@ -1,1 +1,1 @@
-zip -x ".git/*" ".github/*" ".vscode/*" "Auth/*" "Infrastructure/*" README.md .DS_Store .funcignore .gitignore local.settings.json src.zip -r ./src.zip .
+zip -x ".git/*" ".github/*" ".vscode/*" "Auth/*" "Infrastructure/*" README.md .DS_Store .funcignore .gitignore local.settings.json src.zip update-src-zip.sh -r ./src.zip .
